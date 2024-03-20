010963270e32718f57156b4763610a27ce618325
Toshihiro Araki <toaraki@redhat.com>
2024-03-20 23:38:11 +0900

    test commit

--- app.py
+++ app.py
@@ -1,14 +1,41 @@
-from flask import Flask
-import os
+# These are the necessary import declarations
+from opentelemetry import trace
+from opentelemetry import metrics
+
+from random import randint
+from flask import Flask, request
+import logging
+
+# Acquire a tracer
+tracer = trace.get_tracer("diceroller.tracer")
+# Acquire a meter.
+meter = metrics.get_meter("diceroller.meter")
+
+# Now create a counter instrument to make measurements with
+roll_counter = meter.create_counter(
+    "dice.rolls",
+    description="The number of rolls by roll value",
+)
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+@app.route("/rolldice")
+def roll_dice():
+    # This creates a new span that's the child of the current one
+    with tracer.start_as_current_span("roll") as roll_span:
+        player = request.args.get('player', default = None, type = str)
+        result = str(roll())
+        roll_span.set_attribute("roll.value", result)
+        # This adds 1 to the counter for the given roll value
+        roll_counter.add(1, {"roll.value": result})
+        if player:
+            logger.warn("{} is rolling the dice: {}", player, result)
+        else:
+            logger.warn("Anonymous player is rolling the dice: %s", result)
+        return result
 
-if __name__ == '__main__':
-    port = os.environ.get('FLASK_PORT') or 8080
-    port = int(port)
+def roll():
+    return randint(1, 6)
 
-    app.run(port=port,host='0.0.0.0')
