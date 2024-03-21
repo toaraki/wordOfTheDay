e2b187314a35615e171b494a068e4f5980a579ae
Toshihiro Araki <toaraki@redhat.com>
2024-03-21 15:14:27 +0900

    update config

--- runLocal.sh
+++ runLocal.sh
@@ -2,4 +2,5 @@
 
 export OTEL_PYTHON_LOGGING_AUTO_INSTRUMENTATION_ENABLED=true
 export OTEL_EXPORTER_OTLP_TRACES_ENDPOINT="http://otel.jaeger.svc:4317" 
-opentelemetry-instrument --logs_exporter otlp flask run -p 8080
+export OTEL_SERVICE_NAME=rolldice_$(date +'%y%m%d%H%M%S')
+opentelemetry-instrument --log_level debug --logs_exporter otlp flask run -p 8080
